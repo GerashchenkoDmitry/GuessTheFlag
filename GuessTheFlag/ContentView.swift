d208edaeee5d75f9866353eed2c9e3cdb38ce971
Dmitry Gerashchenko <finiqw@gmail.com>
2020-11-10 17:35:03 +0700

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Дмитрий Геращенко on 10.11.2020.
+//  Copyright © 2020 Дмитрий Геращенко. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
